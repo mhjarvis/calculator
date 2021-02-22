45aecb1da7e2eceff28df4fa914381b0ebf6a3a7
mhjarvis <markus.h.jarvis@gmail.com>
2021-02-22 02:14:05 -0700

    Added main functions and calculator layout

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+function addN(a, b) {           //returns sum of two numbers
+    return a + b;
+}
+
+function subtractN(a, b) {      //returns difference of two numbers
+    return a - b;
+}
+
+function multiplyN(a, b) {      //returns product of two numbers
+    return a * b;
+}
+
+function divideN(a, b) {        //returns division of two numbers
+    return a / b;
+}
+
+function operate(opp, a, b) {   //calls add function
+    return addN(a, b);
+}
