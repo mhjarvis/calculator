2bf6b5f5176d8715c0f6405f5e7931411309cfe9
mhjarvis <markus.h.jarvis@gmail.com>
2021-02-22 18:08:32 -0700

    Updated buttons, main arith functions, display functions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+let int1 = 0;
+let int2 = 0;
+let opperator = '';
+const display = document.getElementById("displayP");
 
 
 function addN(a, b) {           //returns sum of two numbers
@@ -19,4 +22,44 @@ function divideN(a, b) {        //returns division of two numbers
 
 function operate(opp, a, b) {   //calls add function
     return addN(a, b);
+}
+
+function displayNumber(char) {
+    int1 = int1 * 10 + char;
+    display.textContent = int1;
+}
+
+function displayOpperator(opp) {
+    opperator = opp;
+    int2 = int1;
+    int1 = 0;
+    display.textContent = opp;
+}
+function displayDecimal(dec) {
+    int1 *= 1.0;
+}
+
+function displayResult() {
+    
+    switch(opperator) {
+        case '+':
+            display.textContent = addN(int2, int1);
+            break;
+        case '-':
+            display.textContent = subtractN(int2, int1);
+            break;
+        case '/':
+            display.textContent = divideN(int2, int1);
+            break;
+        case '*':
+            display.textContent = multiplyN(int2, int1);
+            break;
+    }
+}
+
+function clearAll() {
+    int1 = 0;
+    int2 = 0;
+    opperator = '';
+    display.textContent = 0;
 }
