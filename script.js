b2d2b8b25fac446477b625e5393841c7ffeaee61
mhjarvis <markus.h.jarvis@gmail.com>
2021-02-23 00:06:53 -0700

    Updates

--- script.js
+++ script.js
@@ -1,65 +1,98 @@
-let int1 = 0;
-let int2 = 0;
+let tempNumber = '';
+let displayNum = '';
+let firstNumber = '';
+let secondNumber = '';
 let opperator = '';
+let answer = 0;
+let count = 0;
+
 const display = document.getElementById("displayP");
+const subDisplay = document.getElementById("sub-display");
 
-
-function addN(a, b) {           //returns sum of two numbers
-    return a + b;
+//returns sum of two numbers
+function add(a, b) {
+    int1 = a + b;
+    return int1;
 }
-
-function subtractN(a, b) {      //returns difference of two numbers
-    return a - b;
+//returns difference of two numbers
+function subtract(a, b) {
+    int1 = a - b;
+    return int1;
 }
-
-function multiplyN(a, b) {      //returns product of two numbers
-    return a * b;
+//returns product of two numbers
+function multiply(a, b) {
+    int1 = a * b;
+    return int1;
 }
-
-function divideN(a, b) {        //returns division of two numbers
-    return a / b;
+//returns division of two numbersv
+function divide(a, b) {
+    int1 = a / b;
+    return int1;
 }
-
-function operate(opp, a, b) {   //calls add function
-    return addN(a, b);
-}
-
-function displayNumber(char) {
-    int1 = int1 * 10 + char;
-    display.textContent = int1;
-}
-
-function displayOpperator(opp) {
-    opperator = opp;
-    int2 = int1;
-    int1 = 0;
-    display.textContent = opp;
-}
-function displayDecimal(dec) {
-    int1 *= 1.0;
-}
-
-function displayResult() {
-    
-    switch(opperator) {
-        case '+':
-            display.textContent = addN(int2, int1);
-            break;
-        case '-':
-            display.textContent = subtractN(int2, int1);
-            break;
-        case '/':
-            display.textContent = divideN(int2, int1);
-            break;
-        case '*':
-            display.textContent = multiplyN(int2, int1);
-            break;
-    }
-}
-
+//reset all variables (clear)
 function clearAll() {
-    int1 = 0;
-    int2 = 0;
+    tempNumber = '';
+    firstNumber = '';
+    secondNumber = '';
+    displayNum = '';
     opperator = '';
     display.textContent = 0;
+    subDisplay.textContent = "";
+}
+//displays
+function displayNumber(num) {
+    tempNumber += num;
+    displayNum += num + " ";
+    display.textContent = tempNumber;
+}
+//adds opperator to the display
+function displayOpperator(char) {
+    opperator = char;
+    firstNumber = tempNumber;
+    displayNum += char + " ";
+    tempNumber = '';   
+    subDisplay.textContent = firstNumber + " " + opperator;
+    display.textContent = tempNumber;
+}
+//main display output
+function displayResult() {
+    secondNumber = tempNumber;
+    subDisplay.textContent = firstNumber + " " + opperator + " " + secondNumber + " =";
+    operate(opperator, firstNumber, secondNumber);
+}
+//add decimal to number
+function addDecimal() {
+    tempNumber += ".";
+    displayNum += ".";
+    display.textContent = tempNumber;
+}
+//remove last digit from primary display
+function deleteLast() {
+    tempNumber = tempNumber.slice(0, -1);
+    displayNum = displayNum.slice(0, -1);
+    display.textContent = tempNumber;
+}
+//calls appropriate math function
+function operate(opp, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch(opperator) {
+        case '+':
+            answer = add(a, b);
+            display.textContent = answer;
+            break;
+        case '-':
+            answer = subtract(a, b);
+            display.textContent = answer;
+            break;
+        case '/':
+            answer = divide(a, b);
+            display.textContent = answer;
+            break;
+        case '*':
+            answer = multiply(a, b);
+            display.textContent = answer;
+            break;
+    }
 }
